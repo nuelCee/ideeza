099c193d22849aa8c0e0886b7eb091b72dbf37da
Emmanuel Chinaecherem Nwosu <nwosuchinaecherem@gmail.com>
2020-05-19 15:51:12 -0400

    Overview

--- pages/index/dash.vue
+++ pages/index/dash.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="md:mt-0 md:ml-6 pt-20 w-full pr-3 pl-3 md:pr-6 md:pl-0">
-    <div class="bg-white w-full h-full hidden md:block rounded-sm shadow-lg">
-      <div class="md:flex w-full h-10 items-center justify-center py-12 flow">
-        <div style="transform: translateY(15%);" class="flex flex-col w-2 md:w-10 items-center">
+    <div class="bg-white w-full h-screen md:block rounded-sm shadow-lg">
+      <div
+        class="flex md:flex w-full h-10 items-center justify-center py-0 p-6 pt-10 md:py-12 flow"
+      >
+        <div style="transform: translateY(15%);" class="flex flex-col w-6 md:w-10 items-center">
           <div
-            class="bg-black h-6 w-6 md:h-12 md:w-12 rounded-full act cirlc items-center flex flex-col justify-center"
+            class="bg-black h-12 w-12 rounded-full act cirlc items-center flex flex-col justify-center"
           >
             <img src="../../assets/icons/overview_icon.svg" />
           </div>
           <div class="act_text flow_text">Overview</div>
         </div>
-        <div class="h-1 w-10 md:h-2 md:w-20 bg-black rect"></div>
+        <div class="h-2 w-20 bg-black rect"></div>
         <div style="transform: translateY(15%);" class="flex flex-col w-10 items-center">
           <div
-            class="bg-black h-6 w-6 md:h-12 md:w-12 rounded-full cirlc items-center flex flex-col justify-center"
+            class="bg-black h-12 w-12 rounded-full cirlc items-center flex flex-col justify-center"
           >
             <img src="../../assets/icons/cart_white.svg" />
           </div>
@@ -56,26 +58,26 @@
           <div class="flow_text">Confirmation</div>
         </div>
       </div>
-      <div class="p-10">
+      <div class="p-5 pt-20 md:p-10">
         <div class="w-full">
-          <span class="float-left text-base font-bold">Lamborghini Aventado and Something</span>
+          <span class="float-left text-sm md:text-base font-bold">Lamborghini Aventado and Something</span>
           <img src="../../assets/icons/settings_icon.svg" class="float-right" width="15px" />
         </div>
         <br />
         <br />
         <hr />
-        <div class="p-3 overflow-auto">
+        <div class="p-3 overflow-x-scroll md:overflow-auto table_con">
           <table class="table-fixed w-full">
             <thead>
               <tr class="text-left">
                 <th class="w-1 pr-10 py-2">
                   <div class="w-4 h-4 border-2 rounded-sm"></div>
                 </th>
-                <th class="md:w-1/4 md:px-4 md:py-2">Product Name</th>
-                <th class="w-1/4 px-4 py-2">Category</th>
-                <th class="w-1/4 px-4 py-2">Size</th>
-                <th class="w-1/8 px-4 py-2">Quantity</th>
-                <th class="w-1/6 py-2"></th>
+                <th class="w-56 md:w-1/4 md:px-4 md:py-2">Product Name</th>
+                <th class="w-56 md:w-1/4 px-4 py-2">Category</th>
+                <th class="w-56 md:w-1/4 px-4 py-2">Size</th>
+                <th class="w-1/8 md:w-1/8 px-4 py-2">Quantity</th>
+                <th class="w-56 md:w-1/6 py-2"></th>
               </tr>
             </thead>
             <tbody>
@@ -91,7 +93,7 @@
                 <td class="px-4 py-2">30mm x 60mm x 70mm</td>
                 <td class="px-4 py-2">Adam</td>
                 <td class="px-4 py-2">
-                  <img src="../../assets/icons/more.svg" width="3%" class="float-right" />
+                  <img src="../../assets/icons/more.svg" width="3%" class="float-right w-1" />
                 </td>
               </tr>
               <tr>
@@ -106,31 +108,31 @@
                 <td class="px-4 py-2">30mm x 60mm x 70mm</td>
                 <td class="px-4 py-2">Adam</td>
                 <td class="px-4 py-2">
-                  <img src="../../assets/icons/more.svg" width="3%" class="float-right" />
+                  <img src="../../assets/icons/more.svg" width="3%" class="float-right w-1" />
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
-         <div class="w-full">
-          <span class="float-left text-base font-bold">Lamborghini Aventado and Something</span>
+        <div class="w-full mt-10 md:mt-0">
+          <span class="float-left text-sm md:text-base font-bold">Lamborghini Aventado and Something</span>
           <img src="../../assets/icons/settings_icon.svg" class="float-right" width="15px" />
         </div>
         <br />
         <br />
         <hr />
-        <div class="p-3">
+        <div class="p-3 overflow-x-scroll md:overflow-auto table_con">
           <table class="table-fixed w-full">
             <thead>
               <tr class="text-left">
                 <th class="w-1 pr-10 py-2">
                   <div class="w-4 h-4 border-2 rounded-sm"></div>
                 </th>
-                <th class="w-1/4 px-4 py-2">Product Name</th>
-                <th class="w-1/4 px-4 py-2">Category</th>
-                <th class="w-1/4 px-4 py-2">Size</th>
-                <th class="w-1/8 px-4 py-2">Quantity</th>
-                <th class="w-1/6 py-2"></th>
+                <th class="w-56 md:w-1/4 md:px-4 md:py-2">Product Name</th>
+                <th class="w-56 md:w-1/4 px-4 py-2">Category</th>
+                <th class="w-56 md:w-1/4 px-4 py-2">Size</th>
+                <th class="w-1/8 md:w-1/8 px-4 py-2">Quantity</th>
+                <th class="w-56 md:w-1/6 py-2"></th>
               </tr>
             </thead>
             <tbody>
@@ -146,20 +148,73 @@
                 <td class="px-4 py-2">30mm x 60mm x 70mm</td>
                 <td class="px-4 py-2">Adam</td>
                 <td class="px-4 py-2">
-                  <img src="../../assets/icons/more.svg" width="3%" class="float-right" />
+                  <img src="../../assets/icons/more.svg" width="3%" class="float-right w-1" />
                 </td>
               </tr>
             </tbody>
           </table>
+        </div>
+        <div class="w-full mt-10">
+          <div class="float-left text-sm cont-s">Continue Shopping</div>
+          <div class="float-right text-sm nxt">Next Step
+            <img src="../../assets/icons/arrow_next.svg" class="float-right w-4 ml-2">
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
+.nxt{
+  padding: 10px;
+  background-color: #ff00c7;
+  border-radius: 5px;
+  font-size: 12px;
+  color: white;
+  font-weight: bold;
+}
+.cont-s{
+  padding: 10px;
+  border: 2px solid #ff00c7;
+  color: #ff00c7;
+  border-radius: 5px;
+  font-size: 12px;
+  font-weight: bold;
+}
+.table_con::-webkit-scrollbar {
+  width: 5px;
+  height: 5px;
+}
+
+.table_con::-webkit-scrollbar-track {
+  background-color: #eee;
+  height: 2px;
+  border-radius: 100px;
+}
+
+.table_con::-webkit-scrollbar-thumb {
+  background-color: #ff00c7;
+  border-radius: 100px;
+}
 @media only screen and (max-width: 600px) {
-.cirlc img {
-  transform: scale(0.5)!important;
+  .cirlc img {
+    transform: scale(0.5) !important;
+  }
+  .nxt{
+  padding: 6px;
+  background-color: #ff00c7;
+  border-radius: 5px;
+  font-size: 12px;
+  color: white;
+  font-weight: bold;
+}
+.cont-s{
+  padding: 5px;
+  border: 2px solid #ff00c7;
+  color: #ff00c7;
+  border-radius: 5px;
+  font-size: 12px;
+  font-weight: bold;
 }
 }
 table {
