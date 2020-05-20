76525828c975defd89ea4b43a1dbe27b0004de73
Emmanuel Chinaecherem Nwosu <nwosuchinaecherem@gmail.com>
2020-05-19 20:37:34 -0400

    Dashboard Updated & Cart Added

---
+++ components/item_cart.vue
@@ -0,0 +1,73 @@
+<template>
+  <tr>
+    <td>
+      <div class="w-4 h-4 border-2 rounded-sm"></div>
+    </td>
+    <td class="px-4 py-2 flex items-center">
+      <img :src="src" width="20%" class="float-left" />
+      <div class="ml-3">Lamboghini</div>
+    </td>
+    <td class="px-4 py-2">Electronics, Cover</td>
+    <td class="px-4 py-2">30mm x 60mm x 70mm</td>
+    <td class="px-4 py-2">
+      <div class="flex items-center justify-center w-12">
+        <div class="mr-4 ml-4 text-lg t-col">+</div>
+        <div class="w-2">1</div>
+        <div class="mr-4 ml-4 text-lg t-col">-</div>
+      </div>
+    </td>
+    <td class="px-4 py-2">$20,000</td>
+    <td class="px-4 py-2">
+      <img
+        @click="showMore = !showMore"
+        src="../assets/icons/more.svg"
+        width="3%"
+        class="float-right w-1 opts"
+      />
+      <div
+        v-if="showMore"
+        class="bg-white p-2 md:mt-6 md:ml-6 md:absolute shadow-md rounded-md opte"
+      >
+        <span class="t-col">Project Details</span>
+        <br />
+        <br />
+        <span>Edit choosen manufacture</span>
+        <br />
+        <br />
+        <span>Edit shipping method</span>
+        <br />
+        <br />
+        <span>Delete</span>
+      </div>
+    </td>
+  </tr>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showMore: false
+    }
+  },
+  props: {
+    src: String
+  }
+}
+</script>
+<style scoped>
+.t-col {
+  color: #ff00c7;
+}
+.opts {
+  cursor: pointer;
+}
+@media only screen and (max-width: 600px) {
+  .opte {
+    z-index: 1000;
+    position: absolute;
+    right: 0;
+    margin-right: 50px;
+    margin-top: 20px;
+  }
+}
+</style>
--- pages/index/cart.vue
+++ pages/index/cart.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="md:mt-0 md:ml-6 pt-20 w-full pr-3 pl-3 md:pr-6 md:pl-0">
+    <div class="w-full h-full hidden absolute bg-black left-0 top-0 flex justify-center items-center" style="z-index:999">
+        <div class="w-64 h-64 bg-white"></div>
+    </div>
     <div class="bg-white w-full h-full pb-6 md:block rounded-sm shadow-lg">
       <div
         class="flex md:flex w-full h-10 items-center justify-center py-0 p-6 pt-10 md:py-12 flow"
       >
         <div style="transform: translateY(15%);" class="flex flex-col w-6 md:w-10 items-center">
           <div
-            class="bg-black h-12 w-12 rounded-full act cirlc items-center flex flex-col justify-center"
+            class="bg-black h-12 w-12 rounded-full cirlc items-center flex flex-col justify-center"
           >
-            <img src="../../assets/icons/overview_icon.svg"/>
+            <img src="../../assets/icons/overview_icon.svg" />
           </div>
-          <div class="act_text flow_text">Overview</div>
+          <div class="flow_text">Overview</div>
         </div>
         <div class="h-2 w-20 bg-black rect"></div>
         <div style="transform: translateY(15%);" class="flex flex-col w-10 items-center">
           <div
-            class="bg-black h-12 w-12 rounded-full cirlc items-center flex flex-col justify-center"
+            class="bg-black h-12 w-12 rounded-full act cirlc items-center flex flex-col justify-center"
           >
             <img src="../../assets/icons/cart_white.svg" />
           </div>
-          <div class="flow_text">Cart</div>
+          <div class="act_text flow_text">Cart</div>
         </div>
         <div class="h-2 w-20 bg-black rect"></div>
         <div style="transform: translateY(15%);" class="flex flex-col w-10 items-center">
@@ -58,6 +61,19 @@
           <div class="flow_text">Confirmation</div>
         </div>
       </div>
+      <div class="w-full h-10 px-10 md:mt-6 mt-20">
+        <div class="float-right">
+          <div class="float-left mr-2 text-sm p-1">Optimize by:</div>
+          <select class="text-sm border p-1">
+            <option>Factory Cost</option>
+            <option>Delivery Cost</option>
+            <option>All Costs</option>
+            <option>Factory Time</option>
+            <option>Delivery Time</option>
+            <option>All Time</option>
+          </select>
+        </div>
+      </div>
       <div class="p-5 pt-20 md:p-10">
         <div class="w-full">
           <span class="float-left text-sm md:text-base font-bold">Lamborghini Aventado and Something</span>
@@ -76,12 +92,13 @@
                 <th class="w-56 md:w-1/4 md:px-4 md:py-2">Product Name</th>
                 <th class="w-56 md:w-1/4 px-4 py-2">Category</th>
                 <th class="w-56 md:w-1/4 px-4 py-2">Size</th>
-                <th class="w-1/8 md:w-1/8 px-4 py-2">Quantity</th>
+                <th class="w-56 md:w-1/6 px-4 py-2">Quantity</th>
+                <th class="w-1/8 md:w-1/8 px-4 py-2">Cost</th>
                 <th class="w-56 md:w-1/6 py-2"></th>
               </tr>
             </thead>
             <tbody>
-              <item v-for="(num, index) in 2" src="_nuxt/assets/icons/car_pic.svg" :key="index" />
+              <itemCart v-for="(num, index) in 2" src="_nuxt/assets/icons/car_pic.svg" :key="index" />
             </tbody>
           </table>
         </div>
@@ -102,19 +119,31 @@
                 <th class="w-56 md:w-1/4 md:px-4 md:py-2">Product Name</th>
                 <th class="w-56 md:w-1/4 px-4 py-2">Category</th>
                 <th class="w-56 md:w-1/4 px-4 py-2">Size</th>
-                <th class="w-1/8 md:w-1/8 px-4 py-2">Quantity</th>
+                <th class="w-56 md:w-1/6 px-4 py-2">Quantity</th>
+                <th class="w-1/8 md:w-1/8 px-4 py-2">Cost</th>
                 <th class="w-56 md:w-1/6 py-2"></th>
               </tr>
             </thead>
             <tbody>
-              <item src="_nuxt/assets/icons/car_pic.svg" :key="index" />
+              <itemCart src="_nuxt/assets/icons/car_pic.svg" :key="index" />
             </tbody>
           </table>
         </div>
+        <div class="w-full font-bold mt-10">
+          <span>
+            Total Cost
+            <span class="t-col">$45,000</span>
+          </span>
+        </div>
         <div class="w-full mt-10 flex items-center justify-between">
+          <div class="text-sm nxt">
+            Back Step
+            <img src="../../assets/icons/arrow_next.svg" class="float-left w-4 mr-2 transform rotate-180" />
+          </div>
           <div class="text-sm cont-s">Continue Shopping</div>
-          <div class="text-sm nxt">Next Step
-            <img src="../../assets/icons/arrow_next.svg" class="float-right w-4 ml-2">
+          <div class="text-sm nxt">
+            Next Step
+            <img src="../../assets/icons/arrow_next.svg" class="float-right w-4 ml-2" />
           </div>
         </div>
       </div>
@@ -122,19 +151,19 @@
   </div>
 </template>
 <script>
-import item from '../../components/item'
+import itemCart from '../../components/item_cart'
 
 export default {
   components: {
-    item
+    itemCart
   }
 }
 </script>
 <style scoped>
-.t-col{
+.t-col {
   color: #ff00c7;
 }
-.nxt{
+.nxt {
   padding: 10px;
   background-color: #ff00c7;
   border-radius: 5px;
@@ -142,7 +171,7 @@ export default {
   color: white;
   font-weight: bold;
 }
-.cont-s{
+.cont-s {
   padding: 10px;
   border: 2px solid #ff00c7;
   color: #ff00c7;
@@ -169,22 +198,22 @@ export default {
   .cirlc img {
     transform: scale(0.5) !important;
   }
-  .nxt{
-  padding: 6px;
-  background-color: #ff00c7;
-  border-radius: 5px;
-  font-size: 12px;
-  color: white;
-  font-weight: bold;
-}
-.cont-s{
-  padding: 5px;
-  border: 2px solid #ff00c7;
-  color: #ff00c7;
-  border-radius: 5px;
-  font-size: 12px;
-  font-weight: bold;
-}
+  .nxt {
+    padding: 6px;
+    background-color: #ff00c7;
+    border-radius: 5px;
+    font-size: 12px;
+    color: white;
+    font-weight: bold;
+  }
+  .cont-s {
+    padding: 5px;
+    border: 2px solid #ff00c7;
+    color: #ff00c7;
+    border-radius: 5px;
+    font-size: 12px;
+    font-weight: bold;
+  }
 }
 table {
   font-size: 12px;
